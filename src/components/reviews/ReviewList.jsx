608a9ed232bf5e06a8852002b51cdfd9210bb1f1
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-10-06 13:57:56 -0300

    some changes

--- src/components/reviews/ReviewList.jsx
+++ src/components/reviews/ReviewList.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import ReviewItem from "./ReviewItem.jsx";
 import { useReviews } from "../../context/ReviewsContext.jsx";
 import Loader from "../loader.jsx";
+import { Star } from "lucide-react";
+
 
 export default function ReviewList() {
   const { reviews, getReviews, loading } = useReviews();
@@ -10,9 +12,17 @@ export default function ReviewList() {
     getReviews();
   }, []);
 
+  const totalReviews = reviews?.length || 0;
+  const averageRating =
+    totalReviews > 0
+      ? (reviews.reduce((acc, r) => acc + r.rating, 0) / totalReviews).toFixed(1)
+      : 0;
+
+
   return (
     <div className="review-container">
       <h2> 🔍 Reseñas </h2>
+      <p className="review-subtitle"> Reseñas ({totalReviews}) : {averageRating} <Star className="star-title"/> </p>
       <div className="review-list">
         {loading ? (
           <Loader />
