17acaa7f641ce281e0e3310febf53a25395cbc02
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-10-08 13:28:06 -0300

    add some new pics

--- src/components/reviews/ReviewItem.jsx
+++ src/components/reviews/ReviewItem.jsx
@@ -3,18 +3,24 @@ import { Star } from "lucide-react";
 export default function ReviewItem({ review }) {
   const { name, date, rating, comment, photo } = review;
 
-  const formattedDate = new Date(date).toLocaleDateString("es-CL", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+  const formattedDate = new Date(date)
+    .toLocaleDateString("es-CL", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    })
+    .replace(".", "")
+    .toLowerCase();
 
   return (
     <div className="review-item">
       {/* Imagen del producto */}
       <div className="review-item__image">
         <img
-          src={photo || "https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&h=400&w=400" }
+          src={
+            photo ||
+            "https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&h=400&w=400"
+          }
           alt={`Foto del producto reseñado por ${name}`}
         />
       </div>
@@ -23,10 +29,7 @@ export default function ReviewItem({ review }) {
         {/* Estrellas */}
         <div className="review-item__stars">
           {Array.from({ length: 5 }).map((_, i) => (
-            <Star
-              key={i}
-              className={`star ${i < rating ? "active" : ""}`}
-            />
+            <Star key={i} className={`star ${i < rating ? "active" : ""}`} />
           ))}
         </div>
 
