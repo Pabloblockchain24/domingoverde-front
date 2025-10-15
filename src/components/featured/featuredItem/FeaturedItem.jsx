d548f55c050687147a12ed6139f16b7f7fdba96b
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-10-15 12:17:16 -0300

    some changes

--- src/components/featured/featuredItem/FeaturedItem.jsx
+++ src/components/featured/featuredItem/FeaturedItem.jsx
@@ -1,7 +1,11 @@
 export default function FeaturedItem({ img, title, price, category }) {
+
+    const categoryClass = category.toLowerCase().replace(/\s+/g, "-");
+
+
   return (
     <div className="featured-item">
-      <div className="category-badge">{category}</div>
+      <div className={`category-badge ${categoryClass}`}>{category}</div>
       <img src={img} alt={title} loading="lazy" />
       <h3>{title}</h3>
       <p className="price">${price.toLocaleString("es-CL")}</p>
